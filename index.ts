5ff8f56360583e5c80a64e14170c0d562dfeaa45
aaryandewan <49852384+aaryandewan@users.noreply.github.com>
2023-05-03 12:08:49 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -4,10 +4,3 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
-
-
-
-let obj : any = {
-  name: "aryab"
-};
-console.log(obj);
