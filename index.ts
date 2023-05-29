244e1b8393e1f3062ec68863ba525efd90aedfc9
aaryandewan <49852384+aaryandewan@users.noreply.github.com>
2023-05-29 12:24:53 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,9 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+console.log(123);
+
+let a = {};
+
+a.push(123);
